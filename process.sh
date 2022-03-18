7857ad9e0114b2a503deb4791deeee213dbfe5c9
leigh-grigaliunas <leigh.grigaliunas@gmail.com>
2022-03-18 14:48:09 +1000

    Add comments for clarity

--- process.sh
+++ process.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
+#reads useful content into ENTITY and CONTENT variables 
+read_dom () {
+    local IFS=\>
+    read -d \< ENTITY CONTENT
+}
+
+#sets counter for allocating CONTENT to the correct variable
 count=1
 
-while read line; 
-do 
-	if [[ $line =~ .*string.* ]]; 
-	then 
-		tmp=${line#*>}   # remove prefix ending in "_"
-		b=${tmp%<*} 
+#loops through each line in the file
+while read_dom; do 
+    if [[ $ENTITY = "string" ]]; then
+		#if count is one CONTENT contains an alias
 		if [ $count -eq 1 ];
 		then
-			alias=$b
+			alias=$CONTENT
 			((count++))
 		else
-			aliasname=$b
-			echo "alias $aliasname='$alias'" >> .shell_aliases
+		#if count is 2 then CONTENT contains an aliasname
+			aliasname=$CONTENT
+			echo "alias $aliasname='$alias'"
 			count=1
 		fi
-	fi
-done < subs.plist
-
+    fi
+# < is the file being read in by the loop, > is where the output is redirected
+done < subs.plist > aliases.txt
